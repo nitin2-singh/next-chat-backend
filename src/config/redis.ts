7fca568e5f4e87305210e6c474e7c38ef8f05c9f
Nitin-Campus-ready <nitin@campus-ready.com>
2026-06-26 20:02:03 +0530

    fixed redis 3

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,4 @@
 import Redis from "ioredis";
 import "dotenv/config";
 console.log("process.env.REDIS_URL", process.env.REDIS_URL);
-export const redis = new Redis({
-  host: process.env.REDIS_URL,
-  port: 6379,
-});
+export const redis = new Redis(process.env.REDIS_URL!);
